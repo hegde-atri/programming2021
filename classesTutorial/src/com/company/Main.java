8462d4368c6a6b443d59baa2ac383327f9e99225
Atri Hegde <iamatrihegde@gmail.com>
2020-12-30 14:32:37 +0000

    for each for arrays

--- classesTutorial/src/com/company/Main.java
+++ classesTutorial/src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //The variable x is automatically given the next value of nums and the for loop ends when it has cycled through all the values of num, preventing and boundary errors and presenting a more streamlined code
+        int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int sum = 0;
+        for (int x: nums){
+            System.out.println("Value is: " + x);
+            sum+=x;
+        }
+        System.out.println("Total sum is equal to: " + sum);
     }
 }
