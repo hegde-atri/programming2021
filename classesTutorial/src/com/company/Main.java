a74c6d264ff19595a044a29723052bc97770d40b
Atri Hegde <iamatrihegde@gmail.com>
2020-12-31 18:14:55 +0000

    Initial

--- classesTutorial/src/com/company/Main.java
+++ classesTutorial/src/com/company/Main.java
@@ -2,14 +2,21 @@ package com.company;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
+
+    }
+
+
+
+    public static void forEachLoop(){
         //The variable x is automatically given the next value of nums and the for loop ends when it has cycled through all the values of num, preventing and boundary errors and presenting a more streamlined code
         int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         int sum = 0;
         for (int x: nums){
             System.out.println("Value is: " + x);
-            sum+=x;
+            sum+=x; //can stop by using if statement and stopping when the sum is a certain value (by using break)
         }
         System.out.println("Total sum is equal to: " + sum);
     }
